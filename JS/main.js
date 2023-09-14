9fab2d6f36e1a037a46177b7ac927c40e94be39b
DANBaez23 <andresbaez_23@outlook.com>
2023-09-13 21:25:36 -0500

    se agregan cambios al footer que no se guardaron

--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,7 @@ const cerrar = document.querySelector("#cerrar");
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
 })
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
